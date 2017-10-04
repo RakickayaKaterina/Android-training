2f4136ce85758541006eee8000d3d3689e18ee1e
RakickayaKaterina <ultrareality023@gmail.com>
2017-10-05 00:59:58 +0300

    deleted some log

--- app/src/main/java/com/github/rakickayakaterina/testproject/MainActivity.java
+++ app/src/main/java/com/github/rakickayakaterina/testproject/MainActivity.java
@@ -13,8 +13,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Log.d(TAG,"Some message");
-        Log.d(TAG,"Some text");
-
     }
 
     @Override
