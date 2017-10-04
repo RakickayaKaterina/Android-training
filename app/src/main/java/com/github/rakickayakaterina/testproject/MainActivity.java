6bb39c52730f9ec3f1ee7c9e9e6bbaea2003e990
RakickayaKaterina <ultrareality023@gmail.com>
2017-10-04 23:43:26 +0300

    added Log

--- app/src/main/java/com/github/rakickayakaterina/testproject/MainActivity.java
+++ app/src/main/java/com/github/rakickayakaterina/testproject/MainActivity.java
@@ -2,12 +2,19 @@ package com.github.rakickayakaterina.testproject;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
+    public static final String TAG = "MainActivity";
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(final Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Log.d(TAG,"Some text");
+
     }
+
 }
